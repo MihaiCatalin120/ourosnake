248677048316362b8a95876daecd56f054a8f8fd
Mihai Catalin Toderita <mihaicatalin449@gmail.com>
2026-01-10 19:50:32 +0200

    Change render method

--- main.c
+++ main.c
@@ -11,17 +11,15 @@
 #define GRID_CELL_SIZE 100
 #define DEBUG_MODE true
 
-void updateSnakePosition(Vector2 *head, int *grid, int x, int y,
-                         Color *currentPosColor, int position) {
+void updateSnakePosition(Vector2 *head, int *grid, int x, int y, int position) {
   printf("update snake called \n");
   if ((int)head->x == x && (int)head->y == y) {
     if (grid[position] == 0)
       grid[position] = 8;
-    else
-      WindowShouldClose();
+    // else
+    //   WindowShouldClose();
   }
   if (grid[position] > 0) {
-    *currentPosColor = GetColor(0x33FF3380);
     grid[position] -= 1;
   }
 
@@ -32,14 +30,22 @@ void updateSnakePosition(Vector2 *head, int *grid, int x, int y,
   if (head->y > WINDOW_HEIGHT * 1.0f / GRID_CELL_SIZE)
     head->y = 0;
 
-  // if (DEBUG_MODE)
-  //   printf("[DEBUG]: Head is now x:%f y:%f\n", head->x, head->y);
+  if (DEBUG_MODE)
+    printf("[DEBUG]: Head is now x:%f y:%f\n", head->x, head->y);
 }
 
 void DrawGrid2D(Vector2 *head, int *grid, bool shouldUpdatePositions) {
+  for (size_t x = 0; x < WINDOW_WIDTH / GRID_CELL_SIZE; x += 1) {
+    DrawLine(x * GRID_CELL_SIZE, 0, x * GRID_CELL_SIZE, WINDOW_HEIGHT, WHITE);
+  }
+
+  for (size_t y = 0; y < WINDOW_HEIGHT / GRID_CELL_SIZE; y += 1) {
+    DrawLine(0, y * GRID_CELL_SIZE, WINDOW_WIDTH, y * GRID_CELL_SIZE, WHITE);
+  }
+
   for (size_t y = 0; y < WINDOW_HEIGHT / GRID_CELL_SIZE; y += 1) {
     for (size_t x = 0; x < WINDOW_WIDTH / GRID_CELL_SIZE; x += 1) {
-      Color currentPosColor = GetColor(0x3333FFFF);
+      // Color currentPosColor = GetColor(0x3333FFFF);
       const int gridPosTranslated = y * WINDOW_WIDTH + x;
 
       if (DEBUG_MODE) {
@@ -50,13 +56,8 @@ void DrawGrid2D(Vector2 *head, int *grid, bool shouldUpdatePositions) {
         // grid[gridPosTranslated]);
       }
 
-      if (shouldUpdatePositions) {
-        updateSnakePosition(head, grid, x, y, &currentPosColor,
-                            gridPosTranslated);
-      }
-
-      DrawRectangle(x * GRID_CELL_SIZE + 1, y * GRID_CELL_SIZE + 1,
-                    GRID_CELL_SIZE - 1, GRID_CELL_SIZE - 1, currentPosColor);
+      // DrawRectangle(x * GRID_CELL_SIZE + 1, y * GRID_CELL_SIZE + 1,
+      //               GRID_CELL_SIZE - 1, GRID_CELL_SIZE - 1, currentPosColor);
 
       if (DEBUG_MODE) {
         // Debug text to show grid values, comment if not used
@@ -76,8 +77,14 @@ void DrawGrid2D(Vector2 *head, int *grid, bool shouldUpdatePositions) {
 int main() {
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Hello");
   SetTargetFPS(60);
-  int *grid =
-      calloc(WINDOW_WIDTH * WINDOW_HEIGHT / GRID_CELL_SIZE, sizeof(int));
+  int *grid = (int *)MemAlloc(WINDOW_WIDTH * WINDOW_HEIGHT / GRID_CELL_SIZE *
+                              sizeof(int));
+
+  for (size_t y = 0; y < WINDOW_HEIGHT / GRID_CELL_SIZE; y += 1) {
+    for (size_t x = 0; x < WINDOW_WIDTH / GRID_CELL_SIZE; x += 1) {
+      grid[y * WINDOW_WIDTH + x] = 0;
+    }
+  }
   Vector2 head = {1, 1};
   float time = 0;
 
@@ -85,23 +92,23 @@ int main() {
     float dt = GetFrameTime();
     bool shouldUpdatePositions = false;
     time += dt;
-    if (DEBUG_MODE) {
-      printf("[DEBUG]: Delta - %f\n", dt);
-      printf("[DEBUG]: Time - %f\n", time);
-    }
+    // if (DEBUG_MODE) {
+    //   printf("[DEBUG]: Delta - %f\n", dt);
+    //   printf("[DEBUG]: Time - %f\n", time);
+    // }
     BeginDrawing();
-    DrawFPS(700, 100);
     ClearBackground(GetColor(0x202020FF));
     if (time > 1.0f) {
-      time -= 1.0f;
+      time = 0.0f;
       shouldUpdatePositions = true;
     }
     DrawGrid2D(&head, grid, shouldUpdatePositions);
+    DrawFPS(700, 100);
     shouldUpdatePositions = false;
     EndDrawing();
   }
 
   CloseWindow();
-  free(grid);
+  MemFree(grid);
   return 0;
 }
